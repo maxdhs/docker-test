7db763aa97e53ab7864e5fb0954e832e9cfdd71e
maxdhs <maxdhs@gmail.com>
2022-12-25 16:52:29 -0500

    ..

--- runner.js
+++ runner.js
@@ -2,6 +2,7 @@ const util = require("node:util");
 const exec = util.promisify(require("node:child_process").exec);
 
 async function lsExample() {
+  console.log("starting to run container");
   const { stdout, stderr } = await exec("docker run test");
   console.log("stdout:", stdout);
   console.error("stderr:", stderr);
